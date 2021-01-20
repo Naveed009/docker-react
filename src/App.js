a60da873d03d3eb89538b6c2637ae60402abff13
Naveed009 <naveedmughal1208@gmail.com>
2021-01-20 15:51:09 +0500

    change in App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          aaj kam ni chal rha
+          Alhumdulilah First App on Amazon...
         </p>
         <a
           className="App-link"
